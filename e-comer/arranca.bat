3d9bf3539bdfcaee39528c37c540de2e630c9c26
elgael06 <elgael06@gmail.com>
2019-05-25 09:11:25 -0600

    se agrego alta inventario,alta proveedor, y sus apis

---
+++ e-comer/arranca.bat
@@ -0,0 +1,3 @@
+color 2
+echo "sistema web"
+python C:\www\python\MyEcomer\e-comer\manage.py runserver 0.0.0.0:900
---
+++ e-comer/run.sh
@@ -0,0 +1,6 @@
+source /home/administrador/programacion/python/proyecto/e-comer/virtual/Scripts/activate
+
+cd /home/administrador/programacion/python/proyecto/e-comer/
+
+python3 manage.py runserver 0.0.0.0:8000
+
