31782b1df5898a6da170696d86efb6b06e2c37da
Cristian Gael Valenzuela Castro <elgael06@gmail.com>
2019-06-06 07:44:39 -0600

    Cambio de Interface de Usuario

--- e-comer/arranca.bat
+++ e-comer/arranca.bat
@@ -1,3 +1,4 @@
 color 2
 echo "sistema web"
-python C:\www\python\MyEcomer\e-comer\manage.py runserver 0.0.0.0:900
+C:\programacion\python\Ecomer\e-comer\virtual\Scripts\activate
+python3 C:\programacion\python\Ecomer\e-comer\manage.py runserver 0.0.0.0:900
