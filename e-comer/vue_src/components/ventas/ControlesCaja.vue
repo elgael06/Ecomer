7aeed4fe9edb64ec5f41e29c7fdbb3e20c78e2bc
elgael06 <elgael06@gmail.com>
2019-06-06 14:36:51 -0600

    cambio colores barra superior , menu y fondo

--- e-comer/vue_src/components/ventas/ControlesCaja.vue
+++ e-comer/vue_src/components/ventas/ControlesCaja.vue
@@ -1,92 +1,92 @@
 
 <template>
-    <div class="panel-heading">
-        <div class="row">
-            <div class="col-sm-2">
-                <label>N. Ticket</label>
-                <i class="form-control">{{datos.ticket}}</i>
-            </div>
-            <div class="col-sm-4">
-                <label>Cliente</label>
-                <i class="form-control">{{datos.cliente}}</i>
-            </div>
-            <div class="col-sm-2">
-                <label>Cantidad</label>
-                <i class="form-control">{{datos.cantidad}}</i>
-            </div>
-            <div class="col-sm-2">
-                <label>$total</label>
-                <i class="form-control">$ {{datos.total}}</i>
-            </div>
-            <div class="col-sm-2">
-                <label>$Desc.</label>
-                <i class="form-control">$ {{datos.descuento}}</i>
-            </div>
-            <hr />
-            <form class="col-sm-3" @submit.prevent="buscar_producto">
-                <h4>Producto</h4>
-                <input type="text" id="entrada_producto" class="form-control" v-model="id_producto" />
-            </form>
-            <div class="col-sm-6">
-                <h4>Acciones</h4>
-                <i class="btn btn-default btn-sm fa fa-search" @click="seleccionar_producto"> Buscar</i>
-                <i class="btn btn-warning btn-sm fa fa-user" @click="seleccinar_cliente"> Cliente</i>
-                <i class="btn btn-success btn-sm fa fa-money" @click="pagar_cobro"> Cobrar</i>
-                <i class="btn btn-danger btn-sm fa fa-trash" @click="cancelar_cobro"> Cancelar</i>
-            </div>
-            <div class="col-sm-3">
-                <h4>Fecha</h4>
-                <input type="text" class="form-control" v-model="datos.fecha" disabled />
-            </div>
-        </div>
+  <div class="card-header bg-primary text-white">
+    <div class="row">
+      <div class="col-sm-2">
+        <label>N. Ticket</label>
+        <i class="form-control">{{datos.ticket}}</i>
+      </div>
+      <div class="col-sm-4">
+        <label>Cliente</label>
+        <i class="form-control">{{datos.cliente}}</i>
+      </div>
+      <div class="col-sm-2">
+        <label>Cantidad</label>
+        <i class="form-control">{{datos.cantidad}}</i>
+      </div>
+      <div class="col-sm-2">
+        <label>$total</label>
+        <i class="form-control">$ {{datos.total}}</i>
+      </div>
+      <div class="col-sm-2">
+        <label>$Desc.</label>
+        <i class="form-control">$ {{datos.descuento}}</i>
+      </div>
+      <hr>
+      <form class="col-sm-3" @submit.prevent="buscar_producto">
+        <h4>Producto</h4>
+        <input type="text" id="entrada_producto" class="form-control" v-model="id_producto">
+      </form>
+      <div class="col-sm-6">
+        <h4>Acciones</h4>
+        <i class="btn btn-primary btn-sm fa fa-search" @click="seleccionar_producto">Buscar</i>
+        <i class="btn btn-warning btn-sm fa fa-user" @click="seleccinar_cliente">Cliente</i>
+        <i class="btn btn-success btn-sm fa fa-money" @click="pagar_cobro">Cobrar</i>
+        <i class="btn btn-danger btn-sm fa fa-trash" @click="cancelar_cobro">Cancelar</i>
+      </div>
+      <div class="col-sm-3">
+        <h4>Fecha</h4>
+        <input type="text" class="form-control" v-model="datos.fecha" disabled>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'ControlesCaja',
-    props:['datos','agregar_producto'],
-    data() {
-        return {
-            id_producto:'',
+  name: "ControlesCaja",
+  props: ["datos", "agregar_producto"],
+  data() {
+    return {
+      id_producto: ""
+    };
+  },
+  methods: {
+    buscar_producto() {
+      console.log("Buscar...");
+      this.id_producto == "" || this.obtener_producto();
+    },
+    seleccionar_producto() {
+      console.log("Seleccionar...");
+    },
+    seleccinar_cliente() {
+      console.log("Cliente...");
+    },
+    pagar_cobro() {
+      console.log("Pagar...");
+    },
+    cancelar_cobro() {
+      console.log("Cancelar Cobro...");
+    },
+    obtener_producto() {
+      fetch(`/Compras/productos/api?id=${this.id_producto}`, {
+        method: "get",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json"
         }
-    },
-    methods:{
-        buscar_producto(){
-            console.log("Buscar...");
-            this.id_producto=="" || this.obtener_producto();
-        },
-        seleccionar_producto(){
-            console.log("Seleccionar...");            
-        },
-        seleccinar_cliente(){
-            console.log("Cliente...");
-        },
-        pagar_cobro(){
-            console.log("Pagar...");
-        },
-        cancelar_cobro(){
-            console.log("Cancelar Cobro...");
-        },
-        obtener_producto(){
-            fetch(`/Compras/productos/api?id=${this.id_producto}`,{
-                 method: 'get',
-                credentials: 'same-origin',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            .catch()
-            .then(e=>{
-                e.json().then(res=>{
-                    this.id_producto ='';
-                    console.log(res);
-                    this.agregar_producto(res.producto);
-                })
-            })
-        },
-    },
-}
+      })
+        .catch()
+        .then(e => {
+          e.json().then(res => {
+            this.id_producto = "";
+            console.log(res);
+            this.agregar_producto(res.producto);
+          });
+        });
+    }
+  }
+};
 </script>
 
 
--- e-comer/vue_src/components/ventas/caja.vue
+++ e-comer/vue_src/components/ventas/caja.vue
@@ -1,119 +1,116 @@
 
 
 <template>
-    <div class="panel panel-info">
-        <ControlesCaja v-bind:datos="cavecera_caja" v-bind:agregar_producto="agregar_producto" />
-            <div class="panel-body">
-        <label>Lista Productos :</label>
-        <div style="height:340px;overflow:auto">
-            <table class="table table-condensed">
-                <thead>
-                    <tr class="warning">
-                        <th>Folio</th>
-                        <th>Descripcion</th>
-                        <th>Costo</th>
-                        <th>Cantidad</th>
-                        <th>Descuento</th>
-                        <th>Total</th>
-                        <th>Accion</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <productoLista v-for="(item,index) in productos_lista" 
-                    v-bind:indicadores="indicadores" 
-                    v-bind:item="item" 
-                    v-bind:index="index" 
-                    v-bind:eliminar="eliminar"
-                    :key="item.folio +'_'+ index"  />
-                </tbody>
-            </table>
-        </div>
+  <div class="card">
+    <ControlesCaja v-bind:datos="cavecera_caja" v-bind:agregar_producto="agregar_producto"/>
+    <div class="card-body">
+      <label>Lista Productos :</label>
+      <div style="height:340px;overflow:auto">
+        <table class="table table-condensed">
+          <thead>
+            <tr class="bg-info text-white">
+              <th>Folio</th>
+              <th>Descripcion</th>
+              <th>Costo</th>
+              <th>Cantidad</th>
+              <th>Descuento</th>
+              <th>Total</th>
+              <th>Accion</th>
+            </tr>
+          </thead>
+          <tbody>
+            <productoLista
+              v-for="(item,index) in productos_lista"
+              v-bind:indicadores="indicadores"
+              v-bind:item="item"
+              v-bind:index="index"
+              v-bind:eliminar="eliminar"
+              :key="item.folio +'_'+ index"
+            />
+          </tbody>
+        </table>
+      </div>
     </div>
-    </div>
+  </div>
 </template>
 <script>
-import ControlesCaja from './ControlesCaja';
-import PrductosCaja from './ProductosCaja';
-import productoLista from './ProductoLista';
+import ControlesCaja from "./ControlesCaja";
+import PrductosCaja from "./ProductosCaja";
+import productoLista from "./ProductoLista";
 
 export default {
-    name:'Caja',
-    components:{
-        ControlesCaja,
-        PrductosCaja,
-        productoLista,
-    },
-    data() {
-        return {
-            cavecera_caja:{
-                ticket:0,
-                id_cliente:0,
-                cliente:'',
-                cantidad:0,
-                total:0,
-                descuento:0,
-                fecha:'2019-06-01',
-            },
-            productos_lista:[],
+  name: "Caja",
+  components: {
+    ControlesCaja,
+    PrductosCaja,
+    productoLista
+  },
+  data() {
+    return {
+      cavecera_caja: {
+        ticket: 0,
+        id_cliente: 0,
+        cliente: "",
+        cantidad: 0,
+        total: 0,
+        descuento: 0,
+        fecha: "2019-06-01"
+      },
+      productos_lista: []
+    };
+  },
+  created() {
+    console.log(" Caja...");
+  },
+  updated() {
+    this.indicadores();
+  },
+  methods: {
+    agregar_producto(producto) {
+      if (producto.id > 0) {
+        let index = this.productos_lista.findIndex(e => e.folio == producto.id);
+        if (index == -1) {
+          this.productos_lista.push({
+            folio: producto.id,
+            descripcion: producto.descripcion,
+            costo: producto.venta,
+            cantidad: producto.cantidad,
+            descuento: 0,
+            total: producto.venta,
+            margen: producto.margen
+          });
+        } else {
+          this.productos_lista[index].cantidad += producto.cantidad;
+          this.productos_lista[index].total +=
+            producto.venta *
+              (1 - this.productos_lista[index].descuento / 100) ||
+            producto.venta;
         }
+      } else alert("No Se Encuentra el producto !!!");
     },
-    created(){
-        console.log(" Caja...");
-
+    eliminar(index) {
+      if (confirm("Eliminar ?")) this.productos_lista.splice(index, 1);
     },
-    updated() {
-        this.indicadores();
-    },
-    methods: {
-        agregar_producto(producto){
-            if(producto.id>0){
-            let index = this.productos_lista.findIndex(e=>e.folio == producto.id);
-                if(index==-1){
-                    this.productos_lista.push({
-                        folio:producto.id,
-                        descripcion:producto.descripcion,
-                        costo:producto.venta,
-                        cantidad:producto.cantidad,
-                        descuento:0,
-                        total:producto.venta,
-                        margen:producto.margen,
-                    });
-                }
-                else {
-                    this.productos_lista[index].cantidad += producto.cantidad;
-                    this.productos_lista[index].total += ((producto.venta *(1-(this.productos_lista[index].descuento/100))) 
-                    || producto.venta);
-                }
-            }
-            else alert("No Se Encuentra el producto !!!")
-        },
-        eliminar(index){
-            if(confirm("Eliminar ?"))
-                this.productos_lista.splice(index,1);
-        },
-        indicadores(){
-            this.cavecera_caja.cantidad = this.productos_lista.length;
-            this.cavecera_caja.total = 0;
-             this.productos_lista.map(e=>{
-                this.cavecera_caja.total +=e.total;
-                return e;
-            });
-            this.cavecera_caja.descuento = 0;
-             this.productos_lista.map(e=>{
-                 let total_neto = parseFloat(e.costo) * e.cantidad;
-                 let descuento = parseFloat( e.descuento/100);
-                 
-                this.cavecera_caja.descuento += ( total_neto * descuento) ;
+    indicadores() {
+      this.cavecera_caja.cantidad = this.productos_lista.length;
+      this.cavecera_caja.total = 0;
+      this.productos_lista.map(e => {
+        this.cavecera_caja.total += e.total;
+        return e;
+      });
+      this.cavecera_caja.descuento = 0;
+      this.productos_lista.map(e => {
+        let total_neto = parseFloat(e.costo) * e.cantidad;
+        let descuento = parseFloat(e.descuento / 100);
 
-                return e;
-            });
+        this.cavecera_caja.descuento += total_neto * descuento;
 
-        }
-    },
-    computed:{
-
+        return e;
+      });
     }
-}
+  },
+  computed: {}
+};
 </script>
 
 
